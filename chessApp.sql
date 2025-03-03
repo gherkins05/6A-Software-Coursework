37630859c75f922f0bd65057745106bcb6bbb502
BayleighGG <147563533+BayleighGG@users.noreply.github.com>
2025-03-03 05:02:36 +0000

    SQL Draft
    Built the initial DB using all information I could extract from the ERD. Once ERD is finalised I will complete implementation.

---
+++ chessApp.sql
@@ -0,0 +1,58 @@
+--for testing--
+IF EXISTS "chessapp"
+    DELETE DATABASE "chessapp";
+
+CREATE DATABASE "chessapp";
+
+
+
+CREATE TABLE 
+    game(
+        game_id SERIAL PRIMARY KEY NOT NULL,
+        white INTEGER NOT NULL, --the ERD states this is FK, what does it relate to?
+        black INTEGER NOT NULL, --the ERD states this is FK, what does it relate to?
+        event VARCHAR(15) NOT NULL,
+        site VARCHAR(15) NOT NULL,
+        date VARCHAR(10) NOT NULL,
+        round SMALLINT NOT NULL,
+        result VARCHAR(7) NOT NULL,
+        time_control VARCHAR(15) NOT NULL,
+        FOREIGN KEY (white) REFERENCES ???(???) ON DELETE CASCADE,
+        FOREIGN KEY (black) REFERENCES ???(???) ON DELETE CASCADE
+        )
+
+CREATE TABLE 
+    tag(
+        tag_id SERIAL PRIMARY KEY NOT NULL,
+        tag VARCHAR(15) NOT NULL
+    )
+
+CREATE TABLE 
+    game_tag(
+        game_id INTEGER NOT NULL,
+        tag_id INTEGER NOT NULL,
+        PRIMARY KEY (game_id, tag_id),
+        FOREIGN KEY (game_id) REFERENCES games(game_id) ON DELETE CASCADE,
+        FOREIGN KEY (tag_id) REFERENCES tag(tag_id) ON DELETE CASCADE
+    )
+
+CREATE TABLE 
+    move(
+        move_id SERIAL PRIMARY KEY NOT NULL,
+        game_id INTEGER NOT NULL,
+        player VARCHAR(5) NOT NULL,
+        move_notation VARCHAR(10) NOT NULL,
+        clock VARCHAR(10) NOT NULL,
+        move_number SMALLINT NOT NULL
+    )
+
+CREATE TABLE
+    account(
+        account_id SERIAL PRIMARY KEY NOT NULL,
+        username VARCHAR(24) NOT NULL,
+        password_hash VARCHAR(60) NOT NULL,
+        email VARCHAR(254)
+    )
+
+
+--functions library?--
