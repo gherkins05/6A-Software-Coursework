9f863ebb98da3c07e43542a0601f9c5ccfac7c2f
T-Rain2207558 <up2207558@myport.ac.uk>
2025-03-26 14:15:09 +0000

    added inserts for game

--- chessApp.sql
+++ chessApp.sql
@@ -21,6 +21,14 @@ CREATE TABLE
         FOREIGN KEY (black) REFERENCES account(account_id) ON DELETE CASCADE
         )
 
+insert into game (game, black, white, site, date, event, round, result, time_control) values (1, 2, 1, '6A-Software-Coursework.com', '6/6/2024', 'Live Chess', 63, 'Lose', 'Bullet');
+insert into game (game, black, white, site, date, event, round, result, time_control) values (2, 2, 1, '6A-Software-Coursework.com', '3/11/2025', 'Live Chess', 51, 'Lose', 'Rapid');
+insert into game (game, black, white, site, date, event, round, result, time_control) values (3, 2, 1, '6A-Software-Coursework.com', '9/13/2024', 'Live Chess', 18, 'Win', 'Blitz');
+insert into game (game, black, white, site, date, event, round, result, time_control) values (4, 2, 1, '6A-Software-Coursework.com', '4/25/2024', 'Live Chess', 19, 'Draw', 'Bullet');
+insert into game (game, black, white, site, date, event, round, result, time_control) values (5, 2, 1, '6A-Software-Coursework.com', '12/24/2024', 'Live Chess', 81, 'Draw', 'Bullet');
+
+
+
 CREATE TABLE 
     tag(
         tag_id SERIAL PRIMARY KEY NOT NULL,
