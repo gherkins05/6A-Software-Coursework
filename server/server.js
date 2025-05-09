ee57e1f597a0845d84c6d3eb3c51170a8cb8c112
T-Rain2207558 <up2207558@myport.ac.uk>
2025-05-09 11:45:38 +0100

    Message

--- server/server.js
+++ server/server.js
@@ -46,16 +46,8 @@ app.get('/login', async(req, res) => { login(req, res); });
 // Logout Page
 app.get('/logout', authenticate(), async (req, res) => { logout(req, res); });
 
-<<<<<<< HEAD
-// Get Username
-app.get('/getUserName', async(req, res) => {getUserName(req, res); })
-
-// Get game
-app.get('/getGame', async(req,res) => {getGame(req, res); })
-=======
 // Register Page
 app.post('/register', async (req, res) => { register(req, res); });
 
 // Initiates server
 app.listen(port, () => console.log(`Server running on port ${port}`));
->>>>>>> origin
